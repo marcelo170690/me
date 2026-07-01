0ac961d51e11efd7e2960ff6b489559dbcaa433b
marcelo <marcelo170690@gmail.com>
2026-07-01 13:27:37 -0400

    fix: use Astro command parameter to set base url for CSS asset routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   site: 'https://marcelo170690.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/me' : '/',
-});
+  base: command === 'build' ? '/me' : '/',
+}));
