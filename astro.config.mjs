9b031f7877aaad50dda688071355462b6e83b29a
marcelo <marcelo170690@gmail.com>
2026-07-01 13:23:43 -0400

    refactor: use relative paths and conditional base url for dynamic environment support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcelo170690.github.io',
-  base: '/me',
+  base: process.env.NODE_ENV === 'production' ? '/me' : '/',
 });
